ba8286d17025e8c160f1ae032fae7e28c6177cd1
Rizwan Ali <rizcube@gmail.com>
2015-03-03 13:54:43 +0000

    added two tests and properties

---
+++ src/Fizzbuzz.js
@@ -0,0 +1,9 @@
+var Fizzbuzz = function() {};
+
+  Fizzbuzz.prototype.isDivisibleByThree = function(number) {
+    return number % 3 == 0;
+  };
+
+  Fizzbuzz.prototype.isNotDivisibleByOne = function(number){
+    return number % 3 == 0;
+  };
